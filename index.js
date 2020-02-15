53b6641c57b17e592b2edfb75b05e1c97b31cc7e
Etienne Maleville <lejournaldeetienne@gmail.com>
2020-02-15 14:18:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,17 +35,6 @@ client.on('guildMemberRemove', user =>{
   
 });
 
-fs.readdir('./Commandes/', (error, f) => {
-  if (error) { return console.error(error); }
-      let commandes = f.filter(f => f.split('.').pop() === 'js');
-      if (commandes.length <= 0) { return console.log('Aucune commande trouvée !'); }
-
-      commandes.forEach((f) => {
-          let commande = require(`./Commandes/${f}`);
-          console.log(`${f} commande chargée !`);
-          client.commands.set(commande.help.name, commande);
-      });
-});
 
 fs.readdir('./Events/', (error, f) => {
   if (error) { return console.error(error); }
