eb5d31d44af5008bbcc5960828ecda2347d01d46
Lecodeurfous <59486270+Lecodeurfous@users.noreply.github.com>
2020-02-23 12:01:20 +0100

    ajout de config plus recommencer index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.commands = new Discord.Collection();
+const {TOKEN, PREFIX} = require("./config");
+
 
 
 client.on('ready', () => {
@@ -8,35 +9,12 @@ client.on('ready', () => {
 });
 
 
-client.on('message', msg => {
-  if (msg.content === 'test') {
-    msg.reply(`5/5`+ user.user.displayAvatarURL);
-  }
-});
-
-
-client.on('guildMemberAdd', user =>{
- let bvnembed = new Discord.RicheEmbed()
-   .setColor("#48fb00")
-   .setAuthor(user.user.username, user.user.displayAvatarURL)
-   .setDescription("Bienvenue " + user + "sur le serveur" + user.guild.name + "!")
-   .setFooter(user.guild.name + "Bot crésibot")
-   user.guild.channels.get("674290099047104513").send(bvnembed);
- 
-  
-});
-
-client.on('guildMemberRemove', user =>{
-  let aurvbed = new Discord.RicheEmbed()
-   .setColor("#48fb00")
-   .setAuthor(user.user.username, user.user.displayAvatarURL)
-   .setDescription("Au revoir" + user.user.username )
-   .setFooter(user.guild.name + "Bot crésibot")
-   user.guild.channels.get("674290099047104513").send(aurvbed);
-  
-});
+client.on(message, msg => {
+  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("Pong!")
+ }).
 
 
 
 
-client.login(process.env.TOKEN);
+
+client.login(TOKEN);
