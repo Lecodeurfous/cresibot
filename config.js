9568ba622fb8e094998bc6ad2e78afdd02cb27da
Lecodeurfous <59486270+Lecodeurfous@users.noreply.github.com>
2020-02-23 12:07:19 +0100

    resolution bug

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-exports.TOKEN = process.env.TOKEN
-exports.PREFIX = "p;"
+exports.PREFIX =  "p!"
--- index.js
+++ index.js
@@ -17,4 +17,4 @@ client.on(message, msg => {
 
 
 
-client.login(TOKEN);
+client.login(process.env.TOKEN);
