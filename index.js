182443711fbd925b5b042f3f9344e0735fa21bf2
Lecodeurfous <59486270+Lecodeurfous@users.noreply.github.com>
2020-02-23 11:41:13 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
-const fs = require('fs');
+
 
 client.on('ready', () => {
   console.log(`connecter avec  ${client.user.tag}!`);
@@ -37,15 +37,6 @@ client.on('guildMemberRemove', user =>{
 });
 
 
-fs.readdir('./Events/', (error, f) => {
-  if (error) { return console.error(error); }
-      console.log(`${f.length} events chargés`);
 
-      f.forEach((f) => {
-          let events = require(`./Events/${f}`);
-          let event = f.split('.')[0];
-          client.on(event, events.bind(null, client));
-      });
-});
 
 client.login(process.env.TOKEN);
