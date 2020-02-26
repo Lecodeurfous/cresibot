d6dea7c8f8527eb65832a1a07e1e5ddac0de1c3a
Lecodeurfous <59486270+Lecodeurfous@users.noreply.github.com>
2020-02-26 17:35:14 +0100

    test embed

---
+++ Text/testembed.js
@@ -0,0 +1,32 @@
+const { Client, RichEmbed } = require('discord.js');
+
+
+const client = new Client();
+
+/**
+ * The ready event is vital, it means that only _after_ this will your bot start reacting to information
+ * received from Discord
+ */
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  
+  if (message.content === 'p!embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+});
+
+// Log our bot in using the token from https://discordapp.com/developers/applications/me
